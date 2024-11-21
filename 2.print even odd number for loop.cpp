3bfa8e67798edfaccab763b8b6ae7e73691d8b71
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-11-21 13:59:03 +0530

    first commit

---
+++ 2.print even odd number for loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+cout<<"enter the number=";
+cin>>a;
+if(a%2==0)
+{
+    cout<<"This is even number ";
+}
+else{
+    cout<<"This is odd number";
+}
+
+}
