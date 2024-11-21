622499a48bafb099553379ce494f145627944b6c
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-11-21 14:41:57 +0530

    first commit

---
+++ 1. if else using statement(Example-1).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+       int a;
+       cout<<"enter the number=";
+       cin>>a;
+       if(a>=18)
+       {
+        cout<<"you are eligible for licence";
+       }
+       else{
+        cout<<"Not eligible for licence";
+       }
+}
